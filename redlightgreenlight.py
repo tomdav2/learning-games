673f2e071fd621a757ee0d57faa0998364788d79
tomdav2 <110460896+tomdav2@users.noreply.github.com>
2022-08-08 17:18:15 +0100

    research area update and carrage return

--- redlightgreenlight.py
+++ redlightgreenlight.py
@@ -2,34 +2,34 @@
 #timers -
 # https://www.udacity.com/blog/2021/09/create-a-timer-in-python-step-by-step-guide.html
 #
+#printing to the same line - 
+# https://itnext.io/overwrite-previously-printed-lines-4218a9563527
 #
-# 
-#  
-# 
-# 
-# 
-# every 5 to 10 seconds you have to type a randomly generated centance if you're unable to you get killed
+#
+#
+#  every 5 to 10 seconds you have to type a randomly generated centance if you're unable to you get killed
 # 
 
 #Chantal_the_Doll
 
 import random
 import time
-#countdown = random.randint(1,5)
 lightcolour = 1
 
+#declares lightcolour globally, plus 1 to make it change colour, timesleep makes it wait a random amount of time RLGL produces redlight or greenlight response
 def lightprocess():
     global lightcolour
     lightcolour += 1
-    time.sleep(random.randint(1,5))
+    time.sleep(random.randint(2,15))
     RLGL()
 
+#if the lightcolour is even print red light, if not then print green
 def RLGL():
     if (lightcolour % 2) == 0: 
-        print("red light") 
+        print("red light   ", end='\r') 
         lightprocess()
     else:
-        print ("green light")
+        print ("green light   ", end='\r')
         lightprocess()
 
 
