67ca7d73c2eabf2d3dd3f7cc0de182909bb2aedb
tomdav2 <110460896+tomdav2@users.noreply.github.com>
2022-08-09 20:02:31 +0100

    reduced lines and curser control

--- redlightgreenlight.py
+++ redlightgreenlight.py
@@ -12,24 +12,29 @@
 
 #Chantal_the_Doll
 
+import string
 import random
 import time
 lightcolour = 1
+LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence to clear that line
+PREVLINE = '\033[F' # this should go to the start of the previous line
+
+
 
 #declares lightcolour globally, plus 1 to make it change colour, timesleep makes it wait a random amount of time RLGL produces redlight or greenlight response
 def lightprocess():
     global lightcolour
     lightcolour += 1
-    time.sleep(random.randint(2,15))
+    time.sleep(random.randint(1,5))
     RLGL()
 
 #if the lightcolour is even print red light, if not then print green
 def RLGL():
     if (lightcolour % 2) == 0: 
-        print("red light   ", end='\r') 
+        print(PREVLINE, LINE_CLEAR + "red light", end='\n')
         lightprocess()
     else:
-        print ("green light   ", end='\r')
+        print(PREVLINE, LINE_CLEAR + "green light", end='\n')
         lightprocess()
 
 
