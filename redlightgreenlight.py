ddbe1ce3d0afbbb945fef4c48b0a4754867dbf2d
tomdav2 <110460896+tomdav2@users.noreply.github.com>
2022-08-10 17:29:29 +0100

    end of the day moved onto word game

--- redlightgreenlight.py
+++ redlightgreenlight.py
@@ -16,6 +16,23 @@ import time
 import random
 import string
 
+#research area
+#timers -
+# https://www.udacity.com/blog/2021/09/create-a-timer-in-python-step-by-step-guide.html
+#
+#printing to the same line - 
+# https://itnext.io/overwrite-previously-printed-lines-4218a9563527
+#
+# keyboard press - https://www.delftstack.com/howto/python/python-detect-keypress/
+#
+#
+# every 5 to 10 seconds you have to type a randomly generated centance if you're unable to you get killed
+#Chantal_the_Doll
+
+import time
+import random
+import string
+
 
 LINE_FLUSH = '\r\033[K' # flushes previous print
 LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence to clear that line
