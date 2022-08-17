fe46eca0f6116a0e93c9b12356b06c016e8f2aa2
tomdav2 <110460896+tomdav2@users.noreply.github.com>
2022-08-17 12:43:07 +0100

    working word/steps

--- redlightgreenlightword.py
+++ redlightgreenlightword.py
@@ -1,28 +1,27 @@
-
 import keyboard # pip install keyboard
 import random
 import string
 
 
-run = ''.join(random.choice(string.ascii_letters) for i in range (1))
-char = ""
-steps = 0
-
-def died():
-    run = ''.join(random.choice(string.ascii_letters) for i in range (1))
-    char = ""
-    steps = 0
-
-print(run)
-char = input()
+run = ''.join(random.choice(string.ascii_letters) for i in range (1)) #letter generator
+steps = 0 # current steps character is on
 
 
-while True:  # making a loop
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed(run):
-            print("one step closer")
-    except:
-        print("you died")
-        died()
-        break  # if user pressed a key other than the given key the loop will break
+#steps below 20?
+#generate letter
+#show letter
+#ask for input
+#matches letter?
+#move to next step / kill
 
+while steps <20:
+    run = ''.join(random.choice(string.ascii_letters) for i in range (random.randint(1,6)))
+    print(run)
+    char = input()
+    if run == char:
+        steps += 1
+        print(steps)
+    else:
+        print("KILLED")
+        steps = 0
+        print(steps)
