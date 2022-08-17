e1fd283fbbc99249d7251263cc0d6d890735cb0a
tomdav2 <110460896+tomdav2@users.noreply.github.com>
2022-08-17 17:18:52 +0100

    (NW) intergration of scripts

--- redlightgreenlight.py
+++ redlightgreenlight.py
@@ -1,17 +1,4 @@
-
-#research area
-#timers -
-# https://www.udacity.com/blog/2021/09/create-a-timer-in-python-step-by-step-guide.html
-#
-#printing to the same line - 
-# https://itnext.io/overwrite-previously-printed-lines-4218a9563527
-#
-# keyboard press - https://www.delftstack.com/howto/python/python-detect-keypress/
-#
-#
-# every 5 to 10 seconds you have to type a randomly generated centance if you're unable to you get killed
-#Chantal_the_Doll
-
+import keyboard # <- pip install keyboard
 import time
 import random
 import string
@@ -26,26 +13,42 @@ import string
 # keyboard press - https://www.delftstack.com/howto/python/python-detect-keypress/
 #
 #
-# every 5 to 10 seconds you have to type a randomly generated centance if you're unable to you get killed
+# better way of detecting which key to press https://www.pythonforbeginners.com/basics/how-to-detect-keypress-in-python#:~:text=To%20detect%20keypress%2C%20we%20will,is%20pressed%20on%20the%20keyboard.
+#
 #Chantal_the_Doll
-
-import time
-import random
-import string
 
 
 LINE_FLUSH = '\r\033[K' # flushes previous print
 LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence to clear that line
 PREVLINE = '\033[F' # this should go to the start of the previous line
-STEPS = 0
-REDLIGHT = "red light"
-GREENLIGHT = "green light"
+steps = 0
+redLight = "red light"
+greenLight = "green light"
 
 
-while STEPS <20:
-    LIGHTCHANGEINT = (random.randint(1,6))
-    time.sleep(LIGHTCHANGEINT)
-    if (LIGHTCHANGEINT % 2) == 0:
-        print(LINE_FLUSH, REDLIGHT, end='\n')
+while steps <20:
+    lightChangeInt = (random.randint(1,6))
+    time.sleep(lightChangeInt)
+    if (lightChangeInt % 2) == 0:
+        #RedLight - if you type during this you die
+        print(redLight)
+        if keyboard.is_pressed("*"):
+            print("KILLED")
+            print(steps)
     else:
-        print(LINE_FLUSH, GREENLIGHT, end='\n')
+        #GreenLight - enable typing and random typing sequence
+        print(greenLight)
+        run = ''.join(random.choice(string.ascii_letters) for i in range (random.randint(1,6)))
+        print(run)
+        char = input()
+        if run == char:
+            steps += 1
+            print(steps)
+        else:
+            print("KILLED")
+            print(steps)
+
+
+
+print("you've finished")
+
